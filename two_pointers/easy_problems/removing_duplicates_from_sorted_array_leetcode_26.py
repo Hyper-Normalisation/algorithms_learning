0ccea6903da7fd0154b46df2bab1f3d9857d1e36
Liam Kircher <liamkirchers@gmail.com>
2025-09-12 16:25:37 +0000

    Completed an easy two pointers question leetcode 26, removing duplicates from sorted array

---
+++ two_pointers/easy_problems/removing_duplicates_from_sorted_array_leetcode_26.py
@@ -0,0 +1,41 @@
+# An integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+# Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+
+#Return k.
+
+# -----------------------------------------------------------------------------------------------------
+
+# Key Insights:
+# - Use two pointers fast/slow pointers.
+# - fast scans through the array.
+# - slow marks the "position to overwrite" with the next unique value.
+# - Only move slow when a new unique element is found.
+
+
+# -------------------------------------------------------------------------------------------------------
+
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    slow = 0
+    for fast in range(1, len(nums):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+
+    return slow += 1
+
+
+
+
+
+
+
+
+
+
+
